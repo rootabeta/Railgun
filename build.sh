a26474201ae4bbe78ecc52c616c1a4d22169a1ee
rootabeta <rootabeta@protonmail.com>
2024-06-23 21:41:02 -0400

    Fixed pathing for building extension file, reducing resultant file size

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 VERSION=$(cat manifest.json | jaq .version | cut -d '"' -f 2 | tr '.' '_')
 echo "Building Railgun ${VERSION}"
-zip -r -FS release/railgun-${VERSION} * --exclude .git* release/
+zip -r -FS release/railgun-${VERSION} * --exclude .git* release/*
 echo "Build completed"
